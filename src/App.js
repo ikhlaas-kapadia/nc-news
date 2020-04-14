a8ee46cd282b4960b1f2538e6ed1d918181a3c6e
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-04-14 13:01:33 +0100

    converted to class component and added state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Router } from "@reach/router";
+import "./App.css";
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Home from "./components/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    currentUser: "",
+  };
+  render() {
+    const { currentUser } = this.state;
+    return (
+      <div className="App">
+        <Header />
+
+        <Login
+          currentUser={currentUser}
+          loginUser={this.loginUser}
+          logoutUser={this.logoutUser}
+        />
+
+        <Router>
+          <Home path="/" currentUser={currentUser} />
+        </Router>
+      </div>
+    );
+  }
+
+  loginUser = (username) => {
+    this.setState({ currentUser: username });
+  };
+
+  logoutUser = () => {
+    this.setState({ currentUser: "" });
+  };
 }
 
 export default App;
