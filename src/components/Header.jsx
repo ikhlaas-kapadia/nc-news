5f7667c6a2a540d759ed0bb4e5c82ba55e351a74
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-04-14 17:15:20 +0100

    created Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        <span className="First-Letter">N</span>C News
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
