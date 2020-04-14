6498888f177c366860988b950c22095242fa52a6
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-04-14 20:32:13 +0100

    Handled welcome message

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
+import Topics from "./Topics";
 
 const Home = (props) => {
   const { currentUser } = props;
 
   return (
-    <h2>
-      {!currentUser
-        ? `Welcome to NC News Stranger`
-        : `Welcome to NC News ${currentUser}`}
-    </h2>
+    <main>
+      <h2>
+        {!currentUser
+          ? `Welcome to NC News Stranger`
+          : `Welcome to NC News ${currentUser}`}
+      </h2>
+
+      <Topics />
+    </main>
   );
 };
 
