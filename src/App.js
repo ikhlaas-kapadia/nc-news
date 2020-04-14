0142b38089a8090e9e81778e2c8a9eb4a60ae0d1
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-04-14 20:37:05 +0100

    Created router and added two different path to AllArticles componenet

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import "./App.css";
 import Header from "./components/Header";
 import Login from "./components/Login";
 import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import AllArticles from "./components/All-Articles";
+import Topics from "./components/Topics";
+import ArticlesByTopic from "./components/ArticlesByTopic";
 
 class App extends Component {
   state = {
@@ -14,15 +18,16 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-
+        <NavBar />
         <Login
           currentUser={currentUser}
           loginUser={this.loginUser}
           logoutUser={this.logoutUser}
         />
-
+        <Home currentUser={currentUser} />
         <Router>
-          <Home path="/" currentUser={currentUser} />
+          <AllArticles path="/" />
+          <AllArticles path="/articles/:topic" />
         </Router>
       </div>
     );
