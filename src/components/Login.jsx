b85e2a0a6c5230b0862fa99d4a28013716208feb
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-04-14 13:11:57 +0100

    Created Login component with loggin and logout funcitonality

---
+++ src/components/Login.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+  state = {
+    usernames: ["weegembump", "happyamy2016", "jessjelly", "grumpy19"],
+    inputValue: "jessjelly",
+  };
+  render() {
+    const { currentUser } = this.props;
+
+    return (
+      <div>
+        {!currentUser ? (
+          <form action="" onSubmit={this.handleLogin}>
+            <label htmlFor="">
+              Please login to post comments/cast your votes:
+              <input
+                onChange={this.handleInput}
+                type="text"
+                value={this.state.inputValue}
+              ></input>
+            </label>
+            <button>Login</button>
+          </form>
+        ) : (
+          <p>
+            Logged in as {currentUser}{" "}
+            <button onClick={this.handleLogout}>Logout</button>
+          </p>
+        )}
+      </div>
+    );
+  }
+  handleInput = (e) => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  handleLogin = (e) => {
+    e.preventDefault();
+    const { usernames, inputValue } = this.state;
+    const { loginUser } = this.props;
+    usernames.forEach((username) => {
+      if (username === inputValue) {
+        loginUser(inputValue);
+      }
+    });
+  };
+  handleLogout = (e) => {
+    const { logoutUser } = this.props;
+    logoutUser();
+  };
+}
+
+export default Login;
