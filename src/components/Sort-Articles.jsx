96c8fa27dbe39a58bd7162789ca93cb92650abf3
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-04-15 22:18:07 +0100

    Created function to fetch sorted articles and invoked onChange

--- src/components/Sort-Articles.jsx
+++ src/components/Sort-Articles.jsx
@@ -5,8 +5,10 @@ class Sort extends Component {
   state = {
     sort_by: "",
     order: "desc",
+    author: undefined,
   };
   render() {
+    console.log(this.props);
     return (
       <section>
         Sort By:
@@ -22,18 +24,41 @@ class Sort extends Component {
           <option value="asc">Ascending</option>
           <option value="desc">Descending</option>
         </select>
+        <br />
+        <label>
+          Author:
+          <input onChange={this.handleInput} type="text"></input>
+        </label>
+        <button>Search</button>
       </section>
     );
   }
+
+  handleInput = (e) => {
+    console.log(e.target.value);
+  };
+
+  handleClick = (e) => {
+    // this.setState({author:})
+  };
+
   handleChange = (e, criteria) => {
     const { value } = e.target;
     this.setState({ [criteria]: value }, () => {
-      const { sort_by, order } = this.state;
-      const { sortArticles } = this.props;
-      api
-        .getArticles({ sort_by: sort_by, order: order })
-        .then(({ articles }) => sortArticles(articles));
+      this.fetchSortedArticles();
     });
+  };
+  fetchSortedArticles = () => {
+    const { sort_by, order, author } = this.state;
+    const { sortArticles, topic } = this.props;
+    api
+      .getArticles({
+        author: author,
+        topic: topic,
+        sort_by: sort_by,
+        order: order,
+      })
+      .then(({ articles }) => sortArticles(articles));
   };
 }
 
