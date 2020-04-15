4495dcb279cfa5526ec80addb767b06fd785055b
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-04-15 14:47:17 +0100

    Handled click vote to send patch request to api

--- src/components/Voter.jsx
+++ src/components/Voter.jsx
@@ -1,23 +1,41 @@
 import React, { Component } from "react";
+import "../App.css";
+import * as api from "../utils/api";
 
 class Voter extends Component {
   state = {
     optimisticVote: 0,
   };
+
   render() {
     const { optimisticVote } = this.state;
     return (
       <section>
-        <button onClick={() => this.handleVoteclick(1)}>👌</button> Votes:
-        {this.props.votes + optimisticVote}{" "}
-        <button onClick={() => this.handleVoteclick(-1)}>👎</button>
+        <button
+          onClick={() => this.handleVoteclick(-1)}
+          disabled={optimisticVote < 0}
+          className={optimisticVote === -1 ? "Vote-Btn Voted" : "Vote-Btn "}
+        >
+          👎
+        </button>
+        Votes: {this.props.votes + optimisticVote}{" "}
+        <button
+          className="Vote-Btn"
+          onClick={() => this.handleVoteclick(1)}
+          disabled={optimisticVote > 0}
+          className={optimisticVote === 1 ? "Vote-Btn Voted" : "Vote-Btn "}
+        >
+          👍
+        </button>
       </section>
     );
   }
   handleVoteclick = (vote) => {
+    const { id, type } = this.props;
     this.setState((currentState) => {
       return { optimisticVote: currentState.optimisticVote + vote };
     });
+    api.updateVote(id, vote, type);
   };
 }
 
