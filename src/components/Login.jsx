8d310e15cfe9188ad6577a71720d226a74ff3180
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-04-16 11:42:55 +0100

    changed login functionality using drop down menu with usernames

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,49 +1,48 @@
 import React, { Component } from "react";
-
+import "../App.css";
 class Login extends Component {
   state = {
-    usernames: ["weegembump", "happyamy2016", "jessjelly", "grumpy19"],
-    inputValue: "jessjelly",
+    inputValue: null,
   };
   render() {
     const { currentUser } = this.props;
 
     return (
-      <div>
+      <section>
         {!currentUser ? (
-          <form action="" onSubmit={this.handleLogin}>
-            <label htmlFor="">
-              Please login to post comments/cast your votes:
-              <input
-                onChange={this.handleInput}
-                type="text"
-                value={this.state.inputValue}
-              ></input>
-            </label>
-            <button>Login</button>
-          </form>
+          <div className="User-Menu">
+            <select onChange={this.handleInputValue}>
+              Login
+              <option>Select</option>
+              <option value="jessjelly">jessjelly</option>
+              <option value="happyamy2016">happyamy2016</option>
+              <option value="grumpy19">grumpy19</option>
+              <option value="weegembump">weegembump</option>
+            </select>
+            <p>Please login above to post comments/cast your votes:</p>
+          </div>
         ) : (
           <p>
             Logged in as {currentUser}{" "}
             <button onClick={this.handleLogout}>Logout</button>
           </p>
         )}
-      </div>
+      </section>
     );
   }
-  handleInput = (e) => {
-    this.setState({ inputValue: e.target.value });
+  handleInputValue = (e) => {
+    const { value } = e.target;
+    if (value !== "select") {
+      this.setState({ inputValue: value }, () => {
+        this.handleLogin();
+      });
+    }
   };
 
   handleLogin = (e) => {
-    e.preventDefault();
-    const { usernames, inputValue } = this.state;
-    const { loginUser} = this.props;
-    usernames.forEach((username) => {
-      if (username === inputValue) {
-        loginUser(inputValue);
-      }
-    });
+    const { loginUser } = this.props;
+    const { inputValue } = this.state;
+    loginUser(inputValue);
   };
   handleLogout = (e) => {
     const { logoutUser } = this.props;
