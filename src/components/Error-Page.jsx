8535fccba12454fc7f8531205f95c1c54e7f93ba
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-04-16 16:11:57 +0100

    Changed error message structure

--- src/components/Error-Page.jsx
+++ src/components/Error-Page.jsx
@@ -1,19 +1,14 @@
 import React, { Component } from "react";
 
-class ErrorPage extends Component {
-  class = {
-    err: null,
-  };
-  render() {
-    const { status, msg } = this.props;
-    return (
-      <div className="error">
-        <p>
-          {status}: {msg}
-        </p>
-      </div>
-    );
-  }
-}
+const ErrorPage = (props) => {
+  const { status, msg } = props;
+  return (
+    <div className="error">
+      <h4>Oops! Something went wrong.</h4>
+      <p>Status: {status}</p>
+      <p>{msg}</p>
+    </div>
+  );
+};
 
 export default ErrorPage;
