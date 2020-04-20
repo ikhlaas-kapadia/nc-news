7f274f9ee931a6c1bcdf17a40236045a8d4294b8
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-04-20 09:41:45 +0100

    Added classname

--- src/components/Error-Page.jsx
+++ src/components/Error-Page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ErrorPage = (props) => {
   const { status, msg } = props;
   return (
-    <div className="error">
+    <div className="Error">
       <h4>Oops! Something went wrong.</h4>
       <p>Status: {status}</p>
       <p>{msg}</p>
